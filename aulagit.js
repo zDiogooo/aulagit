bcb73607f9b6ce219a9ad8e400c978a62ff652b9
Diogo <diogoleal813@gmail.com>
2025-04-07 15:23:35 -0300

    incluído ordenação da lista

---
+++ aulagit.js
@@ -0,0 +1,5 @@
+let comidas = [`lasanha`, 'pizza', 'sushi', 'hamburguer'];
+console.log(`lista de todas as comidas: ${comidas}`);
+console.log(`primeira comida: ${comidas[0]}`);
+comidas.sort();
+console.log(`comidas ordenadas: ${comidas}`);

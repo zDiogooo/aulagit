72fca4e5b43a6ea97540133b17145a7c7eb1c71b
Diogo <diogoleal813@gmail.com>
2025-04-14 10:55:33 -0300

    git

--- aulagit.js
+++ aulagit.js
@@ -3,3 +3,4 @@ console.log(`lista de todas as comidas: ${comidas}`);
 console.log(`primeira comida: ${comidas[0]}`);
 comidas.sort();
 console.log(`comidas ordenadas: ${comidas}`);
+console.log('fim');
